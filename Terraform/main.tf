5ce966375fc62eca130c4d8848704aa98881140c
Lane Bench <lanebench@gmail.com>
2026-06-22 15:24:46 -0500

    ECS Fargate deployment with Terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,118 @@
+# --- Data sources: borrow the default VPC and its subnets ---
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# --- CloudWatch log group: where the container's logs go ---
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "/ecs/my-nginx"
+  retention_in_days = 7
+}
+
+# --- IAM: task execution role ---
+data "aws_iam_policy_document" "ecs_task_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "task_execution" {
+  name               = "my-nginx-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution" {
+  role       = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# --- ECS cluster ---
+resource "aws_ecs_cluster" "main" {
+  name = "my-nginx-cluster"
+}
+
+# --- Task definition ---
+resource "aws_ecs_task_definition" "app" {
+  family                   = "my-nginx"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.task_execution.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "my-nginx"
+      image     = "119922150432.dkr.ecr.us-east-1.amazonaws.com/my-nginx:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.app.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+# --- Security group ---
+resource "aws_security_group" "app" {
+  name        = "my-nginx-sg"
+  description = "Allow HTTP inbound to nginx task"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- ECS service ---
+resource "aws_ecs_service" "app" {
+  name            = "my-nginx-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.app.id]
+    assign_public_ip = true
+  }
+}
